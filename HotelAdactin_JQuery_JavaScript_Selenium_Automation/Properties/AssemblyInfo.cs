126f56e4a8d8403b1c16a090952958c4e36312de
MujahidAkberAli <mujahidakberali@gmail.com>
2023-04-24 20:24:46 +0500

    Add project files.

---
+++ HotelAdactin_JQuery_JavaScript_Selenium_Automation/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("HotelAdactin_JQuery_JavaScript_Selenium_Automation")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("HotelAdactin_JQuery_JavaScript_Selenium_Automation")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("041ccc7f-0237-405d-b6a6-49b0d91b8773")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
